1fa85e5100b181f1ea565c5caf3cdef0c7b09a61
Radi Akbar <radia@Radis-MacBook-Pro.local>
2023-09-04 12:19:34 -0400

    Adding the super res transformers and vqgan script (still empty)

--- model/muse.py
+++ model/muse.py
@@ -2,11 +2,57 @@ import torch
 from einops import rearrange
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import AutoTokenizer, T5EncoderModel
+from transformers import T5EncoderModel
 
 """
 Transformer components for the MUSE model
 """
+
+class SuperResTransformer(nn.Module):
+    def __init__(self, config) -> None:
+        super(SuperResTransformer, self).__init__()
+
+        self.src_seq_len = config.src_height * config.src_width
+        self.tgt_seq_len = config.tgt_height * config.tgt_height
+        
+        self.transformer = nn.ModuleDict(dict(
+            src_embedding = nn.Embedding(config.src_codebook_sz, config.emb_dim),
+            tgt_embedding = nn.Embedding(config.tgt_codebook_sz, config.emb_dim),
+            src_pos_enc2d = nn.Embedding(config.src_height * config.src_width, config.emb_dim),
+            tgt_pos_enc2d = nn.Embedding(config.tgt_height * config.tgt_width, config.emb_dim),
+            encoder = nn.ModuleList(
+                [EncoderBlock(config.emb_dim, config.nhead, config.dropout) for _ in range(config.n_enc_layer)]
+            ),
+            decoder = nn.ModuleList(
+                [MultiAxisTransformerBlock(config.emb_dim, config.block_size, config.nhead, config.dropout) for _ in range(config.n_dec_layer)],
+            ),
+            ln_f = nn.LayerNorm(config.emb_dim)
+        ))
+        # other components of the transformer model
+        self.dropout = nn.Dropout(config.dropout)
+        self.lm_head = nn.Linear(config.emb_dim, config.tgt_codebook_sz)
+        # the text encoder components from t5
+        self.text_encoder = get_text_encoder(config.t5_model)
+        self.proj_text = nn.Linear(config.text_emb_dim, config.emb_dim)
+
+    def forward(self, low_res, hi_res, text):
+        n, m, device = self.src_seq_len, self.tgt_seq_len, low_res.device
+        # encode and project the text into our model's dimension
+        text_emb = self.text_encoder(text)
+        text_emb = self.proj_text(text_emb.last_hidden_states)
+        # embed the image and inject positional information
+        src_emb = self.transformer.src_embedding(low_res)
+        src_emb = self.dropout(src_emb + self.transformer.src_pos_enc2d(torch.arange(n, device=device)))
+        # pass the source embedding to encoders
+        src_emb = self.transformer.encoder(src_emb)
+        # embed the hi-res tokens
+        tgt_emb = self.transformer.tgt_embedding(hi_res)
+        tgt_emb = self.dropout(tgt_emb + self.transformer.tgt_pos_enc2d(torch.arange(m, device=device)))
+        # pass the target embedding to the transformers
+        tgt_emb = self.transformer.decoder(tgt_emb, src_emb)
+        tgt_emb = self.transformer.ln_f(tgt_emb)
+
+        return self.lm_head(tgt_emb)
 
 class BaseTransformer(nn.Module):
     def __init__(self, config) -> None:
@@ -41,7 +87,36 @@ class BaseTransformer(nn.Module):
         img_emb = self.transformer.decoder(img_emb, text_emb)
         img_emb = self.transformer.ln_f(img_emb)
 
-        return self.lm_head(img_emb)
+        return self.lm_head(img_emb) 
+
+class EncoderBlock(nn.Module):
+    def __init__(self, emb_dim:int, nhead:int, dropout:float) -> None:
+        super(EncoderBlock, self).__init__()
+
+        self.ln1 = nn.LayerNorm(emb_dim)
+        self.self_attn = MultiQueryAttention(emb_dim, nhead, dropout)
+        self.mlp = MLP(emb_dim)
+
+    def forward(self, input):
+        x = input + self.self_attn(self.ln1(input))
+        x = self.mlp(x)
+        return x
+    
+class MultiAxisTransformerBlock(nn.Module):
+    def __init__(self, nchannels:int, block_size:int, nhead:int, dropout:float) -> None:
+        super(MultiAxisTransformerBlock, self).__init()
+
+        self.ln1 = nn.LayerNorm(nchannels)
+        self.self_attn = MultiAxisAttention(nchannels, block_size, nhead, dropout)
+        self.ln2 = nn.LayerNorm(nchannels)
+        self.cross_attn = MultiQueryAttention(nchannels, nhead, dropout)
+        self.mlp = MLP(nchannels)
+
+    def forward(self, input, context):
+        x = input + self.self_attn(self.ln1(input))
+        x = x + self.cross_attn(self.ln2(x), context)
+        x = self.mlp(x)
+        return x
 
 class TransformerBlock(nn.Module):
     """
@@ -55,7 +130,7 @@ class TransformerBlock(nn.Module):
     Returns:
         x: the last hidden state of the transformer block (b, h * w, d)
     """
-    def __init__(self, emb_dim:int, nhead:int, dropout:float) -> None:
+    def __init__(self, emb_dim:int, nhead:int, dropout:float, multi_axis=False) -> None:
         super(TransformerBlock, self).__init__()
 
         self.ln1 = nn.LayerNorm(emb_dim)
@@ -105,7 +180,7 @@ class MultiQueryAttention(nn.Module):
         dropout: the dropout rate
     Args:
         input: the query input (b, s, d)
-        context: If specified, cross-attention with another output. Else it is self-attention (b, t, d)
+        context: if specified, cross-attention with another output. Else it is self-attention (b, t, d)
     Returns:
 
     """
@@ -134,17 +209,17 @@ class MultiQueryAttention(nn.Module):
         context = context if not None else input
 
         q, k, v = (self.to_q(input), *self.to_kv(context).chunk(2, dim=-1))
-        q = rearrange(q, "b n (h d) -> b h n d", h=h) * self.scale
+        q = rearrange(q, "b ... (h d) -> b h ... d", h=h) * self.scale
 
         if self.flash:
             y = F.scaled_dot_product_attention(q, k, v, dropout_p=self.dropout if self.training else 0.0, is_causal=False)
         
         else:
-            sim = torch.einsum("b h n d, b t d -> b h n t", q, k)
+            sim = torch.einsum("b ... n d, b t d -> b ... n t", q, k)
             att = self.attn_dropoutdropout(sim.softmax(dim=-1))
-            y = torch.einsum("b h n t, b t d -> b h n d", att, v)
+            y = torch.einsum("b ... n t, b t d -> b ... n d", att, v)
 
-        y = rearrange(y, "b h n d -> b n (h d)")
+        y = rearrange(y, "b h ... d -> b ... (h d)")
 
         return self.to_o(y)
     
