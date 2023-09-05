5516ef599f80ba888ab292fc90c648e47dee6f92
karman103 <karmanbir103@gamil.com>
2023-09-05 17:31:57 +0530

    Adding requirements.txt & script for stable diffusion datasets

---
+++ stable_diffusion_datset.py
@@ -0,0 +1,13 @@
+def dataset_laion(num):
+    from datasets import load_dataset    
+    if num == "2B":
+        # 2B dataset
+        print("loading 2B dataset from HuggingFace")
+        dataset = load_dataset("laion/laion2B-en", split="train")         
+    elif num == "400M":
+        # 400m dataset
+        print("loading 400M dataset from HuggingFace")
+        dataset = load_dataset("laion/laion400m", split="train")
+    return dataset
+    
+dataset_laion("2B")
